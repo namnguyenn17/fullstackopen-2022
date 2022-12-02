5e9c8dc44dfdcc5445d29f9592ab9333d984978e
Nam Nguyen <ducnam1719@gmail.com>
2022-12-02 09:59:35 +0700

    unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -18,6 +18,19 @@ const App = () => {
 
   const all = good + neutral + bad
 
+  if (all === 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+        <h1>statistics</h1>
+        <div>No feedback given</div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
